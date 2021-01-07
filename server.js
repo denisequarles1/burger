a24951fc36b6abcf9c87813a6c58a84907b1ca5d
denisequarles1 <71291126+denisequarles1@users.noreply.github.com>
2021-01-06 21:57:01 -0500

    Updated files

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
-
-var path = require('path')
-var app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 3000;
 
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+var app = express();
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+// Serve static content for the app from the "public" directory in the application directory.
+//adding __dirname + to public is what allowed me access to the images in assets/img folder
+app.use(express.static(__dirname + "/public"));
 
-app.use(methodOverride('_method'))
-var exphbs = require('express-handlebars');
-app.engine('hbs', exphbs({
-    defaultLayout: 'main'
-}));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// parse application/json
+app.use(bodyParser.json());
 
-var routes = require('./controllers/burgers_controllers.js');
-app.use('/', routes);
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+//sets main.handlebars as the default layout and our view engine as handlebar
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+//import routes and give the server access to them
+var routes = require("./controllers/burgers_controllers.js");
 
-app.listen(PORT);
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
