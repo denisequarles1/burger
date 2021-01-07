6b182b7368dfd3e47c612ed7f59eb820294ae258
denisequarles1 <denise.quarles1@gmail.com>
2021-01-07 00:58:45 -0500

    updated files

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,20 @@ var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+}else {
+
 connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Cricket83d!',
-  database: 'burgers_db'
+
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burger_db"
 });
-};
+}
 
 // Make connection.
-connection.connect(function (err) {
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
