aec7247d4b50a66cc3c76b75b0ead79047fecfeb
denisequarles1 <71291126+denisequarles1@users.noreply.github.com>
2021-01-06 22:16:05 -0500

    Updated files

--- config/connection.js
+++ config/connection.js
@@ -5,22 +5,12 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-<<<<<<< HEAD
 connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Cricket83d!',
   database: 'burgers_db'
 });
-=======
-  connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3000,
-    user: 'root',
-    password: 'Cricket83d!',
-    database: 'burgers_db'
-  });
->>>>>>> 66e5bdda911703ee6f5f8c60694143358c3666ac
 };
 
 // Make connection.
@@ -32,9 +22,5 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
-<<<<<<< HEAD
 // Export connection for our ORM to use.
 module.exports = connection;
-=======
-module.exports = connection;
->>>>>>> 66e5bdda911703ee6f5f8c60694143358c3666ac
