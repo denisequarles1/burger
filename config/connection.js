15b9434e2d9f3d1a80e4cb9b11ff8befae55ac3c
denisequarles1 <71291126+denisequarles1@users.noreply.github.com>
2021-01-06 21:50:31 -0500

    Updated files

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,18 @@
-var mysql = require('mysql');
-var connection;
+// Set up MySQL connection.
+var mysql = require("mysql");
 
+var connection;
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+<<<<<<< HEAD
+connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Cricket83d!',
+  database: 'burgers_db'
+});
+=======
   connection = mysql.createConnection({
     host: 'localhost',
     port: 3000,
@@ -11,14 +20,21 @@ if (process.env.JAWSDB_URL) {
     password: 'Cricket83d!',
     database: 'burgers_db'
   });
+>>>>>>> 66e5bdda911703ee6f5f8c60694143358c3666ac
 };
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
+<<<<<<< HEAD
+// Export connection for our ORM to use.
 module.exports = connection;
+=======
+module.exports = connection;
+>>>>>>> 66e5bdda911703ee6f5f8c60694143358c3666ac
