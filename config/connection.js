381026ff79b02cd8424856b682407ad27f107f95
denisequarles1 <71291126+denisequarles1@users.noreply.github.com>
2021-01-06 23:45:53 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
-  user: 'root',
-  password: 'Cricket83d!',
-  database: 'burgers_db'
+  user: "root",
+  password: "root",
+  database: "burgers_db"
 });
 };
 
