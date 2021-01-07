faadade12ba3f13ba2edada9763f124083473201
denisequarles1 <71291126+denisequarles1@users.noreply.github.com>
2021-01-06 21:54:33 -0500

    Updated file

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,29 @@
-var orm = require('../config/orm.js');
-
-var burger_call = {
-  insert: function(some_val, cb){
-    orm.insert('burgers_tbl', 'burger_name', some_val, function(res){
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+//"burger" is the table name
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
-  read: function(cb){
-    orm.read('burgers_tbl', function(res){
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
-  update: function(user_id, cb){
-    orm.update('burgers_tbl', 'devoured', 1 , 'id', user_id, function(res){
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
-  delete: function(user_id, cb){
-    orm.delete('burgers_tbl', 'id', user_id, function(res){
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
       cb(res);
     });
   }
-}
+};
 
-module.exports = burger_call;
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
