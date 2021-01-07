f3fdde35d721ec752e3c1db065be7141d08106bf
denisequarles1 <denise.quarles1@gmail.com>
2021-01-06 21:47:45 -0500

    updated files

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,26 @@
-var mysql = require('mysql');
-var connection;
+// Set up MySQL connection.
+var mysql = require("mysql");
 
+var connection;
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3000,
-    user: 'root',
-    password: 'Cricket83d!',
-    database: 'burgers_db'
-  });
+connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Cricket83d!',
+  database: 'burgers_db'
+});
 };
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
