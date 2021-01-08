4eb22ebaa51bc70fc4fcb5055e7812f698f03128
denisequarles1 <denise.quarles1@gmail.com>
2021-01-07 22:03:42 -0500

    updated burger and burgers_controller files

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,8 +6,8 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get('/', function(req, res) {
-  burger.all(function(data) {
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -16,35 +16,35 @@ router.get('/', function(req, res) {
   });
 });
 
-router.post('/burgers', function(req, res) {
-  burger.create([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne(["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put('/burgers', function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
+  burger.updateOne(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
       res.status(200).end();
+
     }
-  });
+  );
 });
 
-router.delete('/burgers', function(req, res) {
+router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   burger.delete(condition, function(result) {
@@ -56,6 +56,7 @@ router.delete('/burgers', function(req, res) {
     }
   });
 });
+
 
 // Export routes for server.js to use.
 module.exports = router;
