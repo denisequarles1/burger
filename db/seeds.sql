caf23c441b1d953f7ed2b0f1d530b946067b2f6c
denisequarles1 <denise.quarles1@gmail.com>
2021-01-07 19:56:38 -0500

    updated files

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,6 +2,6 @@
 USE `burgers_db`;
 
 -- insert 3 burgers
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Angus', false, CURRENT_TIMESTAMP);
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Salmon', false, CURRENT_TIMESTAMP);
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Mushroom', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Angus', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Salmon', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Mushroom', false);
