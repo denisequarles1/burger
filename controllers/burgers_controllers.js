1855717221b767043199538e19528b0b5523ab35
denisequarles1 <denise.quarles1@gmail.com>
2021-01-09 20:53:50 -0500

    updated burger controllers

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res) {
   });
 });
 
-router.post('/burgers', function(req, res) {
+router.post('/api/burgers', function(req, res) {
   burger.insertOne([
     'burger_name'
   ], [
@@ -26,7 +26,7 @@ router.post('/burgers', function(req, res) {
   });
 });
 
-router.put('/burgers/:id', function(req, res) {
+router.put('/api/burgers/:id', function(req, res) {
   var condition = 'id = ' + req.params.id;
 
   burger.updateOne({
