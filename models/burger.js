5ac1a241a65ab8edaaa151414298676151cbc27a
denisequarles1 <denise.quarles1@gmail.com>
2021-01-10 02:19:03 -0500

    updated files

--- models/burger.js
+++ models/burger.js
@@ -5,21 +5,21 @@ var orm = require('../config/orm.js');
 var burgers = {
   // Select all burger table entries
   selectAll: function(cb) {
-    orm.selectAll('burgers', function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
 
   // The variables cols and vals are arrays
   insertOne: function(cols, vals, cb) {
-    orm.insertOne('burgers', cols, vals, function(res) {
+    orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
 
   // The objColVals is an object specifying columns as object keys with associated values
   updateOne: function(objColVals, condition, cb) {
-    orm.updateOne('burgers', objColVals, condition, function(res) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
