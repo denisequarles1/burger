f9927c6d91562a0fcd529ff075b788c6edff5daf
denisequarles1 <denise.quarles1@gmail.com>
2021-01-10 02:37:03 -0500

    updated files

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,18 @@
-// Import the ORM to implement functions that will interact with the database
-var orm = require('../config/orm.js');
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
-// Create the burger object
 var burgers = {
-  // Select all burger table entries
   selectAll: function(cb) {
     orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
-
-  // The variables cols and vals are arrays
+  // The variables cols and vals are arrays.
   insertOne: function(cols, vals, cb) {
     orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
-
-  // The objColVals is an object specifying columns as object keys with associated values
   updateOne: function(objColVals, condition, cb) {
     orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
@@ -25,4 +20,5 @@ var burgers = {
   }
 };
 
+// Export the database functions for the controller (catsController.js).
 module.exports = burgers;
