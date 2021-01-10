f620d95552bdeabc3a33544d9b918fb67e749a1f
denisequarles1 <denise.quarles1@gmail.com>
2021-01-10 02:06:42 -0500

    updated files

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,46 +1,47 @@
-var express = require('express');
-var router = express();
+// placeholder for controller code
+var express = require("express");
 
-var burger = require('../models/burger.js');
+var router = express.Router();
 
-router.get('/', function(req, res) {
-  burger.selectAll(function(data) {
+// Import the model (burger.js) to use its database functions.
+var burgers = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burgers.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
-    res.render('index', hbsObject);
+    // console.log(hbsObject);
+    res.render("index", hbsObject);
   });
 });
 
-router.post('/api/burgers', function(req, res) {
-  burger.insertOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function(data) {
-    // console.log(data);
-    res.json({ id: data.insertId });
+router.post("/api/burgers", function(req, res) {
+  burgers.insertOne(["burger_name"], [
+    req.body.name
+  ], function(result) {
+    // Send back the ID of the new burger
+    res.json({ id: result.insertId });
   });
-  //   burger.insertOne('burger_name', req.body.name, function(data) {
-  //     console.log(data);
-
-  //
-  //   });
 });
 
-router.put('/api/burgers/:id', function(req, res) {
-  var condition = 'id = ' + req.params.id;
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-  console.log('condition:', condition);
+  console.log("condition", condition);
 
-  burger.updateOne(
-    {
-      devoured: req.body.devoured
-    },
-    condition,
-    function(result) {
-      if (result.changedRows == 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+  burgers.updateOne({
+    devoured: true
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
     }
-  );
+  });
 });
+
+// Export routes for server.js to use.
 module.exports = router;
