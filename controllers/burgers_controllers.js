9275b744074e7f9b3ed62471fbb403e2d18f03b4
denisequarles1 <denise.quarles1@gmail.com>
2021-01-09 20:17:05 -0500

    updated files

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,43 +1,40 @@
-//Set up Dependencies
+// Pull in required dependencies
 var express = require('express');
 var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
 var burger = require('../models/burger.js');
 
-//get route -> index
+// Create the routes and associated logic
 router.get('/', function(req, res) {
-    res.redirect('/burgers');
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    // console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
 });
 
-router.get('/burgers', function(req, res) {
-    //express callback response by calling burger.selectAllBurger
-    burger.all(function(burger_data) {
-        //wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
-        res.render('index', { burger_data });
-    });
+router.post('/burgers', function(req, res) {
+  burger.insertOne([
+    'burger_name'
+  ], [
+    req.body.burger_name
+  ], function(data) {
+    res.redirect('/');
+  });
 });
 
-//post route -> back to index
-router.post('/burgers/create', function(req, res) {
-    if (req.body.burger_name == '') {
-        console.log('No burger name entered');
-        res.redirect('/');
-    } else {
-        //takes the request object using it as input for buger.addBurger
-        burger.create(req.body.burger_name, function(result) {
-            //wrapper for orm.js that using MySQL insert callback will return a log to console, render back to index with handle
-            console.log(result);
-            res.redirect('/');
-        });
-    }
+router.put('/burgers/:id', function(req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  burger.updateOne({
+    devoured: true
+  }, condition, function(data) {
+    res.redirect('/');
+  });
 });
 
-//put route -> back to index
-router.put('/burgers/update', function(req, res) {
-    burger.update(req.body.burger_id, function(result) {
-        //wrapper for orm.js that using MySQL update callback will return a log to console, render back to index with handle
-        console.log(result);
-        res.redirect('/');
-    });
-});
-
+// Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -1,56 +1,31 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var timeout = require('connect-timeout');
-var morgan = require('morgan');
-var fs = require('fs');
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-//Express static uses whatever domain name/public
-// app.use(express.static(__dirname + '/public'));
-app.use("/static", express.static("public")); //Handlebars likes this one better
-// Timeout
-app.use(timeout(15000));
-app.use(haltOnTimedout);
+// Serve static content for the app from the "public" directory in the application directory.
+//adding __dirname + to public is what allowed me access to the images in assets/img folder
+app.use(express.static(__dirname + "/public"));
 
-function haltOnTimedout(req, res, next) {
-    if (!req.timedout) next();
-}
-
-//Sets up morgan for logging
-// create a write stream (in append mode)
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
-
-// setup the logger (writes to the file)
-app.use(morgan('combined', { stream: accessLogStream }));
-
-//This sets up body-parser
-// parse application/x-www-form-urlencoded 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(haltOnTimedout);
 
-//This is setting up method-override 
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
-app.use(haltOnTimedout);
+// parse application/json
+app.use(bodyParser.json());
 
-//sets up express-handlebars
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+//sets main.handlebars as the default layout and our view engine as handlebar
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-//Setting up the routes
-var routes = require('./controllers/burgers_controllers.js');
+//import routes and give the server access to them
+var routes = require("./controllers/burgers_controllers.js");
 
-app.use('/', routes);
-app.use('/update', routes);
-app.use('/create', routes);
-app.use(haltOnTimedout);
+app.use(routes);
 
-//sets up the port as a variable and listens to it
-var port = process.env.PORT || 3000;
-app.listen(port, () => console.log("Listening on port %s", port));
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
